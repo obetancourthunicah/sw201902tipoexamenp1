197e0a2613c33adb58ec6b18927a4f4a9313db93
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-06-05 18:53:21 -0600

    Implementado Put y Delete

--- tipoexamen/routes/api/personas.js
+++ tipoexamen/routes/api/personas.js
@@ -22,7 +22,7 @@ personaCollection.push(
       nombre:'Orlando J Betancourth',
       correo:'obetancourthunicah@gmail.com',
       telefono: 'jajajaja',
-      notas:'Ejemplo de Persona en ejercicio tipo examen'
+      nota:'Ejemplo de Persona en ejercicio tipo examen'
     }
     )
 );
@@ -50,11 +50,30 @@ router.post('/', (req, res, next)=>{
 
 
 router.put('/:id', (req, res, next)=>{
-  res.status(403).json({ msg: "not implemented" });
+  var id = req.params.id;
+  var modifiedPersona = {};
+  var originalPersona = {};
+  personaCollection = personaCollection.map( (e, i) => {
+    if(e.id === id){
+      originalPersona = Object.assign({}, e);
+      return modifiedPersona = Object.assign({}, e, req.body);
+    }
+    return e;
+  });//map
+  res.status(200).json({ o: originalPersona, m: modifiedPersona});
 });//put
 
 router.delete('/:id', (req, res, next)=>{
-  res.status(403).json({ msg: "not implemented" });
+  var id = req.params.id;
+  var deletedPersona = {};
+  personaCollection = personaCollection.filter((e,i)=>{
+    if(e.id === id){
+      deletedPersona = Object.assign({},e);
+      return false;
+    }
+    return true;
+  });//filter
+  res.status(200).json({d:deletedPersona, c:personaCollection});
 });//delete
 
 module.exports = router;
